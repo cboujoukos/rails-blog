c4196de0851a29093e2857efad4d6a3b44d092f2
Chris Boujoukos <github email address>
2018-09-07 11:33:20 -0400

    set up articlescontroller actions

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :delete]
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      flash.now[:alert] = "You must enter a Title and body"
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
